41ea164380863f1bd129b76ded592684eb626db2
Abdullah Al Kaisan <abdullahalkaisan@gmail.com>
2024-03-03 05:04:43 +0600

    update log card

--- src/components/LogsCard.jsx
+++ src/components/LogsCard.jsx
@@ -1,7 +1,7 @@
-import { Box, Button, IconButton, Typography } from "@mui/material";
+import { Avatar, AvatarGroup, Box, Button, IconButton, Typography } from "@mui/material";
 import PeopleCard from "./conent/rightMenu/PeopleCard";
-// import TranslateIcon from '@mui/icons-material/Translate';
-// import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import TranslateIcon from '@mui/icons-material/Translate';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 // import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
 // import AccessAlarmsIcon from '@mui/icons-material/AccessAlarms';
 // import { UilLetterJapaneseA } from '@iconscout/react-unicons'
@@ -25,75 +25,19 @@ export default function LogsCard() {
       width={"90%"}
       borderRadius={2}
     >
+
+
+
       <Box display={"flex"} flexDirection={"column"} padding={3}>
-        <Box
-          display={"flex"}
-          alignItems={"center"}
-          justifyContent={"space-around"}
-          flexDirection={"row"}
-        >
-          <PeopleCard
-            badge={1}
-            // flag={"bd"}
-            avatar={1}
-            avatarUrl={
-              "https://pbs.twimg.com/profile_images/1544305803888566272/7uAiIOYR_400x400.jpg"
-            }
-            title={"Abdulah Al Kaisan"}
-            subTitle="Web developer"
-          />
-          <Button
-            disabled
-            variant="contained"
-            size="small"
-            sx={{ borderRadius: 999 }}
-          >
-            Join
-          </Button>
-        </Box>
-
-        <Box display={"flex"} alignItems={"center"} marginTop={2}>
-          {/* backgroundColor:"#F5F5F5"  */}
-          {/* marginRight:1  */}
-          {/* #333333  */}
-          <Box>
-            <IconButton
-              sx={
-                isDark
-                  ? { borderRadius: 2, backgroundColor: "#333333" }
-                  : { borderRadius: 2, backgroundColor: "#F5F5F5" }
-              }
-              size="small"
-            >
-              {/* <AccessTimeIcon fontSize="14px" /> */}
-              <Box
-                sx={{
-                  fontSize: "medium",
-                  fontWeight: "bold",
-                  display: "flex",
-                  ml: 0.2,
-                }}
-              >
-                {/* <i className="fi fi-rr-clock-three"></i> */}
-                <Typography variant="caption">
-                  30m - 10:45 AM - 10/12/2024
-                </Typography>
-              </Box>
-            </IconButton>
-          </Box>
-
-          <Box>
-            <Button color="success" size="small">
-              $1.12
-            </Button>
-          </Box>
-        </Box>
 
 
-        {/* FF000010  */}
+
+
+
         <Box>
           <Button
             style={{
+              marginTop: 6,
               fontSize: "samll",
               textTransform: "none",
               backgroundColor: "#FFA50010",
@@ -105,16 +49,95 @@ export default function LogsCard() {
           </Button>
         </Box>
 
+        <Box sx={{ width: "max-content", display: "flex", marginTop: 2 }}>
+          <AvatarGroup max={4}>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://pbs.twimg.com/profile_images/1544305803888566272/7uAiIOYR_400x400.jpg"
+            />
+            <Avatar
+              alt="Travis Howard"
+              src="https://lh3.googleusercontent.com/a/ACg8ocK386pzE7Z-bSFqbGXHYVxxKt5pYh_OAnMnkKWDOPhNabI"
+            />
+            <Avatar
+              alt="Cindy Baker"
+              src="https://lh3.googleusercontent.com/a/ACg8ocKjcLQnbqhz1e6GuIXL1IrBSg2pG_FSAtw-vO1ejOyW890"
+            />
+            <Avatar
+              alt="Agnes Walker"
+              src="https://lh3.googleusercontent.com/a/ACg8ocKg4H0S4oUQl5TVLe72k5iueHshMcheBvx51rZ_jWm84s4"
+            />
+          </AvatarGroup>
+        </Box>
+
+        <Box display={"flex"} alignItems={"center"} marginTop={2}>
+          <Box
+            fontSize="smaller"
+            color={"#777"}
+            display={"flex"}
+            alignItems={"center"}
+          >
+            <AccessTimeIcon sx={{ marginRight: 0.5 }} fontSize="smaller"/>
+            30 minutes - 10:45 AM - 10/12/2024
+          </Box>
+        </Box>
+
+        <Box display={"flex"} alignItems={"center"}>
+          <Box
+            fontSize="smaller"
+            color={"#777"}
+            display={"flex"}
+            alignItems={"center"}
+          >
+            <TranslateIcon sx={{ marginRight: 0.5 }} fontSize="smaller" />
+            English
+          </Box>
+
+          <Box>
+            <Button color="success" size="small">
+              $1.12
+            </Button>
+          </Box>
+        </Box>
+
+
+
+
         <Typography style={{ padding: "10px", fontSize: "medium" }} variant="">
           {`Let's discuss about Javascript`}
         </Typography>
 
-        <Box sx={{display:"flex", width:1, mt:3, justifyContent:"flex-end"}} >
-          <Button color="error" size="small" style={{backgroundColor:"#FF000005",}}  >Reject</Button>
+
+
+
+        <Box
+          sx={{ display: "none", width: 1, mt: 3, justifyContent: "flex-end" }}
+        >
+          <Button
+            color="error"
+            size="small"
+            style={{ backgroundColor: "#FF000005" }}
+          >
+            Reject
+          </Button>
           <Box>
-            <Button size="small" style={{backgroundColor:"#1976D210",}}>Accept</Button>
+            <Button size="small" style={{ backgroundColor: "#1976D210" }}>
+              Accept
+            </Button>
           </Box>
         </Box>
+
+
+        <Box textAlign={"end"}>
+          <Button
+              variant="contained"
+              size="small"
+              sx={{ borderRadius: 999, display:"" }}
+            >
+              Join
+            </Button>
+        </Box>
+
 
 
       </Box>
