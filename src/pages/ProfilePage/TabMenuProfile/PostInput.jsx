8f4ba24f524c077151f39d3c79275b941e8c4988
Abdullah Al Kaisan <40631908+abdullahalkaisan@users.noreply.github.com>
2024-05-18 20:15:23 +0600

    update

--- src/pages/ProfilePage/TabMenuProfile/PostInput.jsx
+++ src/pages/ProfilePage/TabMenuProfile/PostInput.jsx
@@ -25,9 +25,9 @@ export default function PostInput() {
     return (
 
         
-        <Box component="form" onSubmit={inputSubmit} sx={{ p: 4, display: "flex", flexDirection: "column", alignItems: "center",  }}>
+        <Box component="form" onSubmit={inputSubmit} sx={{ p: {md:4, sx:1}, pt:{md:0,xs:4},   display: "flex", flexDirection: "column", alignItems: "center",  }}>
                 <Box sx={{width:"100%", display:"flex", flexDirection:"column", alignItems:"center"}}>
-                    <Box  sx={{ width: "50%", display: "flex",  alignItems:"flex-start", }}>
+                    <Box  sx={{ width:{md:620, xs:1 }, display: "flex",  alignItems:"flex-start", }}>
                         <PeopleCard
                             avatar
                             avatarUrl={"https://pbs.twimg.com/profile_images/1544305803888566272/7uAiIOYR_400x400.jpg"}
@@ -47,9 +47,9 @@ export default function PostInput() {
                             onChange={(e) => { setPostInput(e.target.value) }}
                         />
                     </Box>
-                    <Box sx={{ display: "flex", my: 3, width:"50%", justifyContent:"flex-end" }}>
+                    <Box sx={{ display: "flex", my: 3, width:{md:620, xs:1 }, justifyContent:"flex-end" }}>
                         <Box sx={{display:"flex", width:1, alignItems:"center", justifyContent:"space-between"}}>
-                            <Box marginLeft={6}>
+                            <Box marginLeft={{md:6,sx:0}}>
                                 <Button 
                                 // sx={{backgroundColor:"#f1f1f1", color:"#333", borderRadius:999, boxShadow:"none", "&:hover":{backgroundColor:"#f1f1f1"}}}
                                 sx={{borderRadius:3}}
